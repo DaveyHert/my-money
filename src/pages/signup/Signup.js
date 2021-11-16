359b2c481cfc07ff20499ae9949b9519588250f2
David Herbert <daviehert@gmail.com>
2021-11-16 19:41:06 +0100

    Refactored

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -6,8 +6,16 @@ export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(username);
+    console.log(email);
+    console.log(password);
+  };
+
   return (
-    <form className={style["signup-form"]}>
+    <form className={style["signup-form"]} onSubmit={handleSubmit}>
       <h1>Sign Up Page</h1>
 
       <label>
