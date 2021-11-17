88550f34f23bab02f6030392c1a27eaac1e5870b
David Herbert <daviehert@gmail.com>
2021-11-17 13:30:14 +0100

    refactored home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,18 @@
 import style from "./Home.module.css";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Home Page </h1>
+
+      {user ? (
+        <p>Welcome back {user.displayName}</p>
+      ) : (
+        <p>You are not logged in</p>
+      )}
     </div>
   );
 }
