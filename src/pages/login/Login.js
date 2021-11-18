4ba48a79875e567e34808e0f6c77fc6c1826dd12
David Herbert <daviehert@gmail.com>
2021-11-18 18:23:18 +0100

    Login added

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,15 @@
 import style from "./Login.module.css";
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
@@ -31,7 +33,13 @@ export default function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
-      <button className='btn'>Login</button>
+      {!isLoading && <button className='btn'>Login</button>}
+      {isLoading && (
+        <button className='btn' disabled>
+          Loading...
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 }
