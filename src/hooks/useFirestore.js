465d8e286ddcfc8bee542318d963c02431b4cdf3
David Herbert <daviehert@gmail.com>
2021-11-21 11:20:35 +0100

    Firestore hook added

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,28 +1,80 @@
 import { useState, useReducer, useEffect } from "react";
-import { projectFirestore } from "../firebase/config";
+import { projectFirestore, timeStamp } from "../firebase/config";
 
-// Initial state
+// Initial state (defined outside the hook because we don't want it recreated ech time the hook is used)
 const initialState = {
   document: null,
   isLoading: false,
   error: null,
+  success: null,
 };
 
-const dispatchReducer = (state, action) => {
-    switch (action.type) {
-        case 'ISLOADING':
-            return {...state, isLoading: true, document: null, error: null};
-     case 'ERROR':
-                return {...state, error: action.payload, isLoading: false, document: null};
-        case 'ADD_DOCUMENT':
-                    return{...state, document: action.payload}
-        default:
-            return state
+// A reduce function that manages our state
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    case "IS_LOADING":
+      return { isLoading: true, document: null, error: null, success: false };
+    case "ADD_DOCUMENT":
+      return {
+        isLoading: false,
+        document: action.payload,
+        error: null,
+        success: true,
+      };
+    case "ERROR":
+      return {
+        error: action.payload,
+        isLoading: false,
+        document: null,
+        success: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (collection) => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [response, dispatch] = useReducer(firestoreReducer, initialState);
+
+  //   Collections reference
+  const ref = projectFirestore.collection(collection);
+
+  //  cleanup function (only dispatch if component is not unmounted)
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
     }
-}
+  };
 
-export const useFirestore = (document) => {
-    const [isCancelled, setIsCancelled] = useState(false)
-    const 
+  //   Add to documents
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_LOADING" });
+    try {
+      // Add time
+      const createdAt = timeStamp.fromDate(new Date());
+      // Upload doc
+      const newDocoument = await ref.add({ ...doc, createdAt });
 
-}
+      dispatchIfNotCancelled({ type: "ADD_DOCUMENT", payload: newDocoument });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
+  };
+
+  //   Remove document
+  const deleteDocument = async (id) => {
+    dispatch({ type: "IS_LOADING" });
+    try {
+      const res = await ref.doc(id).delete();
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
+  };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, response, deleteDocument };
+};
