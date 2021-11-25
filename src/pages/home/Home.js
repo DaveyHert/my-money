d2f96eacd8dce26a93486ec573c77a1c52bd3de7
David Herbert <daviehert@gmail.com>
2021-11-25 18:43:28 +0100

    Cleaned home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,26 @@
-import style from "./Home.module.css";
+import styles from "./Home.module.css";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
+import { useCollection } from "../../hooks/useCollection";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection(
+    "transactions",
+    ["uid", "==", user.uid],
+    ["createdAt", "desc"]
+  );
 
   return (
-    <div className={style.container}>
-      <div className='content'>Transaction List</div>
+    <div className={styles.container}>
+      <div className='content'>
+        {documents && <TransactionList transactions={documents} />}
+        {error && <p>{error}</p>}
+        {documents.length === 0 && <p>Your transactions list is empty</p>}
+      </div>
 
-      <div className={style.sidebar}>
+      <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
     </div>
